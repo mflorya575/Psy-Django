7c4dee67d6595e3e465d9bb467b888aa0781dbd6
Michael Florya <mih2303@mail.ru>
2024-07-07 14:28:35 +0300

    add page about

--- psy/main/views.py
+++ psy/main/views.py
@@ -8,3 +8,12 @@ def index(request):
     }
 
     return render(request, 'main/index.html', context)
+
+
+def about(request):
+
+    context = {
+        'title': 'О нас - Психоаналитика',
+    }
+
+    return render(request, 'main/about.html', context)
