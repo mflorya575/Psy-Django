c7ef429f48d0205924a85ddf4e28c96b7cb89356
Michael Florya <mih2303@mail.ru>
2024-07-07 14:26:17 +0300

    config page index (controller, urls, template)

--- psy/main/views.py
+++ psy/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+
+    context = {
+        'title': 'Главная - Психоаналитика',
+    }
+
+    return render(request, 'main/index.html', context)
